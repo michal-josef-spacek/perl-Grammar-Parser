5767bccdcdd6e8be67c2dd63c8bed9b23b67b757
Branislav Zahradník <happy.barney+perl-Grammar-Parser@gmail.com>
2020-02-06 07:55:23 +0100

    CSI test helper

---
+++ t/test-helper-csi.pl
@@ -0,0 +1,112 @@
+#!/usr/bin/env perl
+
+use v5.14;
+use strict;
+use warnings;
+
+use FindBin;
+use lib $FindBin::Bin;
+
+use Grammar::Parser;
+use CSI::Language::Java::Grammar;
+use CSI::Grammar::Actions;
+
+BEGIN { require "test-helper-common.pl" }
+
+our $DUMP_IT_GOT = 1;
+our $DUMP_IT_EXPECTED = 1;
+
+contrive 'csi-parser-start' => (
+	deduce  => 'csi-language',
+	builder => 'start_rule',
+);
+
+contrive 'csi-parser-grammar' => (
+	deduce  => 'csi-language',
+	builder => 'grammar',
+);
+
+contrive 'csi-parser-action-map' => (
+	deduce  => 'csi-language',
+	builder => 'actions',
+);
+
+contrive 'csi-parser-insignificant' => (
+	deduce  => 'csi-language',
+	builder => 'insignificant_rules',
+);
+
+contrive 'csi-parser-action-lookup' => (
+	deduce  => 'csi-language',
+	builder => 'action_lookup',
+);
+
+contrive 'csi-parser' => (
+	class => 'Grammar::Parser',
+	dep => {
+		grammar       => 'csi-parser-grammar',
+		action_lookup => 'csi-parser-action-lookup',
+		action_map    => 'csi-parser-action-map',
+		start         => 'csi-parser-start',
+		insignificant => 'csi-parser-insignificant',
+	},
+);
+
+my $last_result;
+act {
+	$last_result = deduce ('csi-parser')->parse (@_);
+	$last_result;
+};
+
+sub arrange_start_rule {
+	my ($rule) = @_;
+
+	proclaim 'csi-parser-start' => $rule;
+}
+
+sub is_arranged_start_rule {
+	my ($rule) = @_;
+
+	try_deduce 'csi-parser-start';
+	is_deduced 'csi-parser-start';
+}
+
+sub test_rule {
+	my ($title, %params) = @_;
+
+	test_frame {
+		arrange_start_rule $params{rule}
+			if defined $params{rule};
+
+		arrange_start_rule $title
+			unless is_arranged_start_rule;
+
+		$title = $params{title}
+			if exists $params{title};
+
+		it $title => (
+			args => [ $params{data} ],
+			expect => $params{expect},
+		) or do {
+			use DDP;
+			if ($DUMP_IT_GOT) {
+				diag ("== Got");
+				diag (np $last_result);
+			}
+			if ($DUMP_IT_EXPECTED) {
+				diag ("== Expected");
+				diag (np $params{expect});
+			}
+		};
+
+		if ($params{expectation_expanded}) {
+			it "$title (expectation expanded)" => (
+				args => [ $params{data} ],
+				expect => $params{expectation_expanded},
+			);
+		}
+	};
+}
+
+1;
+
