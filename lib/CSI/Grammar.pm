f0772bf030a53146e80d973e6e267a44de16768a
Branislav Zahradník <happy.barney+perl-Grammar-Parser@gmail.com>
2020-03-05 06:02:35 +0100

    CSI::Grammar without attributes

--- lib/CSI/Grammar.pm
+++ lib/CSI/Grammar.pm
@@ -6,14 +6,12 @@ use strict;
 use warnings;
 
 package CSI::Grammar v1.0.0 {
-	use Attribute::Handlers;
 	require Exporter;
 
 	use CSI::Grammar::Meta;
 	use CSI::Grammar::Actions;
 
-	my $annonymous_counter = "000000";
-	my %dom;
+	my $anonymous_counter = "000000";
 
 	sub import {
 		my ($class, @params) = @_;
@@ -31,7 +29,7 @@ package CSI::Grammar v1.0.0 {
 			no strict 'refs';
 
 			my $meta = CSI::Grammar::Meta->new (class => $caller);
-			$meta->prepend_action_lookup ("CSI::Grammar::Actions::__::${\ ++$annonymous_counter }::$class");
+			$meta->prepend_action_lookup ("CSI::Grammar::Actions::__::${\ ++$anonymous_counter }::$class");
 			*{"${caller}::__csi_grammar"} = sub { $meta };
 
 			push @{ "${caller}::ISA" }, __PACKAGE__;
@@ -141,119 +139,6 @@ package CSI::Grammar v1.0.0 {
 
 	sub register_action_lookup {
 		(scalar caller)->__csi_grammar->prepend_action_lookup (@_);
-	}
-
-	sub keyword {
-		my ($name, @def) = @_;
-
-		my $keyword = lc $name;
-		(
-			$name,
-			proto => 'Keyword',
-			@def,
-			eval "qr/(?> \\b $keyword \\b )/sx",
-		);
-	}
-
-	sub _rule_name {
-		my ($class, $symbol, $referent, $attr_name, $attr_data, $phase) = @_;
-		*{ $symbol }{NAME};
-	}
-
-	sub _grammar_element {
-		my ($class, $symbol, $referent, $attr_name, $attr_data, $phase) = @_;
-
-		my $rule_name = _rule_name @_;
-		$class->__csi_grammar->add_rule ($rule_name => $class->$referent);
-	}
-
-	sub _action {
-		my ($class, $symbol, $referent, $attr_name, $attr_data, $phase) = @_;
-
-		my $action = lc $attr_name =~ s/^ACTION_//r;
-
-		$class->__csi_grammar->add_action (_rule_name (@_) => $action);
-	}
-
-	sub RULE                :ATTR(CODE) { &_grammar_element }
-	sub TOKEN               :ATTR(CODE) {
-		my ($class, $symbol, $referent, $attr_name, $attr_data, $phase) = @_;
-
-		_token $class, _rule_name (@_), $class->$referent;
-	}
-
-	sub REGEX               :ATTR(CODE) {
-		my ($class, $symbol, $referent, $attr_name, $attr_data, $phase) = @_;
-
-		_regex $class, _rule_name (@_), $class->$referent;
-	}
-
-	sub ACTION_ALIAS        :ATTR(CODE) { &_action }
-	sub ACTION_DEFAULT      :ATTR(CODE) { &_action }
-	sub ACTION_LIST         :ATTR(CODE) { &_action }
-	sub ACTION_LITERAL      :ATTR(CODE) { &_action }
-	sub ACTION_LITERAL_VALUE:ATTR(CODE) { &_action }
-	sub ACTION_PASS_THROUGH :ATTR(CODE) { &_action }
-	sub ACTION_SYMBOL       :ATTR(CODE) { &_action }
-	sub START               :ATTR(CODE) {
-		my ($class, $symbol, $referent, $attr_name, $attr_data, $phase) = @_;
-
-		_start $class, _rule_name @_;
-
-		&_grammar_element;
-	}
-
-	sub INSIGNIFICANT       :ATTR(CODE) {
-		my ($class, $symbol, $referent, $attr_name, $attr_data, $phase) = @_;
-
-		_insignificant $class, _rule_name @_;
-
-		TOKEN (@_);
-	}
-
-	sub PROTO               :ATTR(CODE) {
-		my ($class, $symbol, $referent, $attr_name, $attr_data, $phase) = @_;
-
-		# TODO: multiple prototypes (TOKEN / RULE)
-		# TODO: TOKEN only prototypes should be expanded as regex groups
-		eval {
-			my $rule_name = _rule_name @_;
-			for my $proto (@$attr_data) {
-				$class->__csi_grammar->append_rule ($proto => \ $rule_name);
-			}
-		};
-
-		if ($@) {
-			say "Attribute handler PROTO";
-			p $attr_data;
-			exit;
-		}
-	}
-
-	sub TRANSFORM           :ATTR(CODE) {
-		my ($class, $symbol, $referent, $attr_name, $attr_data, $phase) = @_;
-
-		# capture group transformation callback
-		my $rule_name = _rule_name @_;
-		return unless $rule_name eq 'LITERAL_STRING';
-		use DDP;
-		p $attr_data;
-
-		my ($key, $transform) = @$attr_data;
-
-		$class->__csi_grammar->add_action (_rule_name (@_) => sub {
-			my ($instance, $name, $token) = @_;
-
-			$token->$key ($class->$transform ($token->$key));
-			p $token;
-			say "Found rule handler: $rule_name";
-			$token;
-		});
-	}
-
-	sub TRAIT               :ATTR(CODE) {
-		# what role has to be applied on result
-		# examples: Literal, Statement, Statement::Loop, Expression, Operator, Separator, ...
 	}
 
 	sub grammar {
