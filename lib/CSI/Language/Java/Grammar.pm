a0fa2eba80f7295ef4c0b38e579f64bd8e31d6a7
Branislav Zahradník <happy.barney+perl-Grammar-Parser@gmail.com>
2020-03-24 00:42:48 +0100

    Language::Java - expression / primary / expression group

--- lib/CSI/Language/Java/Grammar.pm
+++ lib/CSI/Language/Java/Grammar.pm
@@ -601,6 +601,10 @@ package CSI::Language::Java::Grammar v1.0.0 {
 		[qw[  lambda_expression       ]],
 		;
 
+	rule  expression_group                  =>
+		[qw[  PAREN_OPEN  statement_expression  PAREN_CLOSE  ]],
+		;
+
 	rule  field_modifier                    => dom => 'CSI::Language::Java::Modifier',
 		[qw[  annotation  ]],
 		[qw[  private     ]],
@@ -818,6 +822,12 @@ package CSI::Language::Java::Grammar v1.0.0 {
 	rule  reference_type                    =>
 		[qw[  array_type       ]],
 		[qw[  class_type       ]],
+		;
+
+	rule  statement_expression              =>
+		[qw[  assignment                          ]],
+		[qw[  instance_creation_expression        ]],
+		[qw[  expression                          ]],
 		;
 
 	rule  type_argument                     =>
@@ -2047,18 +2057,6 @@ __END__
 			[qw[                  if_then_else_statement ]],
 			[qw[                         while_statement ]],
 			[qw[                           for_statement ]],
-		]
-	}
-
-	sub statement_expression        :RULE :ACTION_PASS_THROUGH {
-		[
-			[qw[                         assignment ]],
-			[qw[           pre_increment_expression ]],
-			[qw[           pre_decrement_expression ]],
-			[qw[          post_increment_expression ]],
-			[qw[          post_decrement_expression ]],
-			[qw[                  method_invocation ]],
-			[qw[ class_instance_creation_expression ]],
 		]
 	}
 
--- t/csi-grammar-java/rule-expression.t
+++ t/csi-grammar-java/rule-expression.t
@@ -11,7 +11,7 @@ BEGIN { require "test-helper-csi-language-java.pl" }
 
 arrange_start_rule 'expression';
 
-plan tests => 4;
+plan tests => 6;
 
 test_rule "primary expression / literal / null" => (
 	data => 'null',
@@ -26,6 +26,26 @@ test_rule "primary expression / literal / integer number" => (
 test_rule "primary expression / class literal" => (
 	data => 'String.class',
 	expect => [ expect_literal_class (expect_reference ([qw[ String ]])) ]
+);
+
+test_rule "primary expression / group expression" => (
+	data => '(null)',
+	expect => [
+		expect_token_paren_open,
+		expect_literal_null,
+		expect_token_paren_close,
+	],
+);
+
+test_rule "primary expression / double-group expression" => (
+	data => '((null))',
+	expect => [
+		expect_token_paren_open,
+		expect_token_paren_open,
+		expect_literal_null,
+		expect_token_paren_close,
+		expect_token_paren_close,
+	],
 );
 
 had_no_warnings;
